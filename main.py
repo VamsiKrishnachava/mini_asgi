6bfeee4f709d363aabe5bdce41ffc1ab4cfe2f44
unknown <chavavamsi29@gmail.com>
2026-07-21 08:52:56 +0530

    First working and usage of framework.

---
+++ main.py
@@ -0,0 +1,11 @@
+from mini_asgi.application import MiniASGI
+
+app = MiniASGI()
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello, World!"}
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the app made using MiniASGI framework!"}
