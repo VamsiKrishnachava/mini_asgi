56ed4f64ad51f847a8278b057ea7a21bd944430c
unknown <chavavamsi29@gmail.com>
2026-07-23 09:45:39 +0530

    Refactor route class.

--- mini_asgi/models/route.py
+++ mini_asgi/models/route.py
@@ -4,13 +4,26 @@ from mini_asgi.models.request import Request
 
 class Route:
     def __init__(self, method, path, func):
-        self.method = method
-        self.path = path
+        self._method = method
+        self._path = path
         self.func = func
         try:
             self.expectedRequestParameter = self._ExpectedRequest(func)
+            self._isAsync = self._isAsync(func)
         except ValueError as e:
             raise ValueError(f"Error in route path : {path}, method {method}: {str(e)}")
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def isAsync(self):
+        return self._isAsync
         
     def _ExpectedRequest(self, function):
         """
@@ -25,3 +38,15 @@ class Route:
                 else:
                     raise ValueError("Function should not have more than one parameter of type Request.")
         return returnParameter
+
+    def _isAsync(self, function):
+        """ 
+            This is a helper function to determine if a function is asynchronous.
+        """
+        if function is None:
+            raise ValueError("Function is not defined correctly.")
+
+        if not callable(function):
+            raise TypeError("Expected a callable.")
+        
+        return inspect.iscoroutinefunction(function)
