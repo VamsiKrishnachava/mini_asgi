ecfa9d0ee313ca87c3eeb3a3784df06d48d33c88
Chava Vamsi Krishna <52667046+VamsiKrishnachava@users.noreply.github.com>
2026-07-24 09:28:01 +0530

    Query params implementation (#5)
    * Upper.
    
    * Basic get, init and call of MINI ASGI.
    
    * First working and usage of framework.
    
    * Extended router functionality to get, put, post, patch and delete methods.
    
    * Added models request, route. Sendig request parammater to functions in call
    
    * Bug fix for route model handling in application
    
    * Merge conflict resolution
    
    * Merge conflict resolution
    
    * Refactor route class.
    
    * Receive bosy in request.
    
    * Query parameters implementation

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ app = MiniASGI()
 def hello():
     return {"message": "Hello, World!"}
 
+@app.post("/json_tester")
+async def json_tester(request : Request):
+    body = await request.json()
+    if request.query_params:
+        return {"message" : f"The json is working correctly. The body.age = {body['age']} and params filter = {request.query_params.get('filter', "Did not get any filter")}"}
+    return {"message" : f"The json is working correctly. The body.age = {body['age']}"}
+
+
 @app.get("/")
 def root(request : Request):
     return {"message": "Welcome to the app made using MiniASGI framework! the method used is: " + request.method + " and the path is: " + request.path}
